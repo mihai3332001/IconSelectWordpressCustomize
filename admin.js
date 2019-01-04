6c1bd8dc16cdc8237f1c4119bf69952012166f4a
mihai3332001 <34579013+mihai3332001@users.noreply.github.com>
2019-01-04 19:57:23 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -32,18 +32,19 @@ $.fn.customIconSelected = function() {
         this.each(function() {
              var obj = $(this);
         var obj_val= obj.val();
-        var select_icon = obj.parents();
+        var select_icon = obj.parents('li');
+        console.log(select_icon);
         var parent = obj.parent().parent();
         var input_icon = parent.children('#icon_select2')
         var input_val = parent.children('#icon_select2').val();
         if(input_val == obj_val) {
            obj.attr('selected', 'selected');
            if(obj_val == 'custom-icon') {  
-              select_icon.find('#customize-control-eveline_frontpage_header_login_custom_icon').show();
-               select_icon.find('#customize-control-eveline_frontpage_header_login_awesome_icon').hide();
+          select_icon.next().hide();
+         select_icon.next().next().show();
            } else {
-            select_icon.find('#customize-control-eveline_frontpage_header_login_awesome_icon').show(); 
-            select_icon.find('#customize-control-eveline_frontpage_header_login_custom_icon').hide();      
+               select_icon.next().show();
+        select_icon.next().next().hide();    
            }
 
         } 
@@ -51,22 +52,22 @@ $.fn.customIconSelected = function() {
     };
 
     $.fn.selectFontIcons = function() {
-        this.change(function() {
+        this.change(function(e) {
              var obj = $(this);
-        var select_icon = obj.parents();
+        var select_icon = obj.parents('li');
         var parent = obj.parent();
         var select_val = obj.children('option:selected').val();
         var input_icon = parent.children('#icon_select2')
         var input_val = parent.children('#icon_select2').val();
-        if(input_val === "custom-icon") {
-            $(input_icon).val(select_val).change();
-          select_icon.find('#customize-control-eveline_frontpage_header_login_custom_icon').hide();
-         select_icon.find('#customize-control-eveline_frontpage_header_login_awesome_icon').show();
-        } else if (input_val === "font-awesome") {
-          var test = $(input_icon).val(select_val).change();
-          //console.log(test);
-          select_icon.find('#customize-control-eveline_frontpage_header_login_custom_icon').show();
-            select_icon.find('#customize-control-eveline_frontpage_header_login_awesome_icon').hide();
+        var change_val = input_icon.val(select_val).change();
+        console.log(select_icon);
+        if(change_val.val() === "custom-icon") {    
+          select_icon.next().hide();
+         select_icon.next().next().show();
+        } else if (change_val.val() === "font-awesome") {
+         
+          select_icon.next().show();
+         select_icon.next().next().hide();
         }
     });
         return this;
@@ -101,10 +102,10 @@ $.fn.customIconSelected = function() {
         });
     };
 
-    $('#iconmoon').customIcon();
-    $('#iconmoon option').customIconSelected();
-    $('#icons-custom option').selectedFontIcon();
-    $('#icons-custom').selectFontIcons();
-    $('#combobox option').fontAwesomeIconSelected();
-    $('#combobox').fontAwesomeIcon();
+    $('.icons-custom option').selectedFontIcon();
+    $('.icons-custom').selectFontIcons();
+    $('.iconmoon').customIcon();
+    $('.iconmoon option').customIconSelected();
+    $('.combobox option').fontAwesomeIconSelected();
+    $('.combobox').fontAwesomeIcon();
    
